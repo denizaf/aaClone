74e5acb9cbf80a9780c8e558087fe72780358878
denizaf <denizafsar98@gmail.com>
2024-05-29 14:56:47 +0300

    changed pin positions to angles

--- Assets/Scripts/LevelData.cs
+++ Assets/Scripts/LevelData.cs
@@ -5,7 +5,7 @@ public class LevelData : ScriptableObject
 {
     public int levelNumber;
     public int pinsRequired;
-    public Vector3[] initialPinPositions;
+    public float[] initialPinAngles;
     public float circleSpeed;
     public float circleAcceleration;
     public bool reverseDirection;
