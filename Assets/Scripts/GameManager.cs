b22b06c72f36a8772b85746c5152fdbd04a1338c
denizaf <denizafsar98@gmail.com>
2024-05-31 14:01:08 +0300

    check conditions for fsm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,20 +9,23 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public GameObject pinPrefab;
-    public Transform pinSpawnPoint;
+    //public GameObject pinPrefab;
+    //public Transform pinSpawnPoint;
     public float restartDelay = 1.5f;
     public float cameraShakeDuration = 0.5f;
     public float cameraShakeMagnitude = 0.1f;
     
     private bool _isGameOver = false;
     private bool _canThrowPin = false;
+    private GameStateController _stateController;
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
+            _stateController = new GameStateController();
         }
         else
         {
@@ -32,18 +35,17 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        _canThrowPin = false;
+        ChangeState(new MenuState(this));
     }
-
+    
     private void Update()
     {
-        if (_isGameOver || !_canThrowPin) return;
-        
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(pinPrefab, pinSpawnPoint.position, Quaternion.identity);
-            _canThrowPin = false;
-        }
+        _stateController.Update();
+    }
+    
+    public void ChangeState(IGameState newState)
+    {
+        _stateController.ChangeState(newState);
     }
 
     public void PinLanded()
@@ -60,13 +62,18 @@ public class GameManager : MonoBehaviour
     {
         _canThrowPin = true;
     }
+    
+    public bool CanThrowPin()
+    {
+        return _canThrowPin;
+    }
 
     public void GameOver()
     {
         //Display game over UI
         //Handle game over Logic
         Debug.Log("Game over.");
-        
+        DisplayGameOverUI();
         Invoke("RestartLevel", restartDelay);
     }
 
@@ -81,11 +88,63 @@ public class GameManager : MonoBehaviour
         Camera.main.GetComponent<CameraController>().ShakeCamera(cameraShakeDuration, cameraShakeMagnitude);
     }
     
-    private void RestartLevel()
+    public void RestartLevel()
     {
         _isGameOver = false;
         _canThrowPin = true;
         LevelManager.Instance.ResetLevel();
+        ChangeState(new PlayingState(this));
+    }
+    
+    public bool CheckGameOverCondition()
+    {
+        return _isGameOver;
+    }
+
+    public bool CheckLevelCompletedCondition()
+    {
+        return LevelManager.Instance.IsLevelCompleted();
+    }
+
+    public bool CheckRestartCondition()
+    {
+        return UIManager.Instance.IsRestartButtonPressed();
+    }
+
+    public bool CheckNextLevelCondition()
+    {
+        return UIManager.Instance.IsNextLevelButtonPressed();
+    }
+    
+    public bool CheckBackToMenuCondition()
+    {
+        return UIManager.Instance.IsMenuButtonPressed();
+    }
+
+    public void DisplayGameOverUI()
+    {
+        UIManager.Instance.ShowGameOverPanel();
+    }
+
+    public void HideGameOverUI()
+    {
+        UIManager.Instance.HideGameOverPanel();
+    }
+
+    public void DisplayLevelCompletedUI()
+    {
+        UIManager.Instance.ShowLevelCompletedPanel();
+    }
+
+    public void HideLevelCompletedUI()
+    {
+        UIManager.Instance.HideLevelCompletedPanel();
+    }
+
+    public void LoadNextLevel()
+    {
+        LevelManager.Instance.LoadNextLevel();
+        ChangeState(new PlayingState(this));
     }
     
     public void SavePlayerStats()
@@ -103,4 +162,13 @@ public class GameManager : MonoBehaviour
         SavePlayerStats();
     }
 
+    public void GoToStartPage()
+    {
+        ChangeState(new MenuState(this));
+    }
+
+    public bool IsGameOver()
+    {
+        return _isGameOver;
+    }
 }
